be9e00dc25e63a486e50b632c61fb429fc007af6
VaibhavBangadkar <103397194+VaibhavBangadkar@users.noreply.github.com>
2022-07-19 01:11:25 +0530

    Microsoft Azure AI chatbot
    Here is project URL link :  https://vaibhavazurechatbot.azurewebsites.net
    Here is the project video demo URL  Link : https://www.youtube.com/watch?v=T68-rsXRFCM

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\vaibhavazurechatbot-bot-ae6b-Web-Deploy.pubxml -p:Password=oYmaPfSFGJtPsAqrW6kbvQHeR6fodwbzd1um0SaR3t8zMYE32gffcltdTFbM
+
